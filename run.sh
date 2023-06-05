87fe3a3924b293cd63968035a65f27eaabe40937
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2023-06-05 16:47:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 #Bulk rename in bash
+
 a=1
 echo -en 'ENTER BASE NAME: '
 read bae
-for i in *.rar; do
-  new=$(printf "$bae%04d.rar" "$a") #04 pad to length of 4
+for i in *.$1; do
+  new=$(printf "$bae%04d.$1" "$a") #04 pad to length of 4
   mv -i -- "$i" "$new"
   let a=a+1
 done
+
+#Command you can use
+#./run.sh ext
+
+#example
+#./run.sh mp3
+#enter the base name
+#done
